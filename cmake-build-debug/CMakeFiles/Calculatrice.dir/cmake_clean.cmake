9dad3696afe2e22325ed360368474838a91fa84f
77Gabriel <44476244+77Gabriel@users.noreply.github.com>
2018-10-25 19:26:56 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Calculatrice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculatrice.dir/main.c.obj"
+  "Calculatrice.pdb"
+  "Calculatrice.exe"
+  "Calculatrice.exe.manifest"
+  "libCalculatrice.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Calculatrice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
